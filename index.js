64067c09184acd10229871e137619165668754f6
haykam821 <haykam821@users.noreply.github.com>
2017-05-19 08:40:44 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName('button')[0].addEventListener('click', function() {
+	document.getElementsByTagName('span')[0].value = parseInt(document.getElementsByTagName('span')[0].value) + 1;
+})
