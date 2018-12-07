3a8c3f3be2e5f08ab96b2eb9ce7b91d7732e2116
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-12-06 19:28:28 -0500

    Use ES6 syntax

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.getElementsByTagName('button')[0].addEventListener('click', function() {
+document.getElementsByTagName('button')[0].addEventListener("click", () => {
 	document.getElementsByTagName('span')[0].value = parseInt(document.getElementsByTagName('span')[0].value) + 1;
-})
+});
